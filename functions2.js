34c19d42aa761887317431c3750aae4da44fd182
unknown <osama.albader99@gmail.com>
2024-10-08 22:39:36 +0300

    taske 2

--- functions2.js
+++ functions2.js
@@ -1,41 +1,13 @@
-/**
- * isOdd(n):
- * - receives a number n
- * - returns true if it's odd, false otherwise
- *
- * e.g.
- * isOdd(7) -> true
- * isOdd(10) -> false
- */
 function isOdd(n) {
   if (n % 2 === 1) return true;
   else return false;
 }
 
-/**
- * oddsSmallerThan(n):
- * - receives a number n
- * - returns the number of ODD numbers smaller than n
- *
- * e.g.
- * oddsSmallerThan(7) -> 3
- * oddsSmallerThan(15) -> 7
- */
 function oddsSmallerThan(n) {
   if (isOdd(n) === true) consol.log((n + 1) / 2);
   else return n;
 }
 
-/**
- * squareOrDouble(n):
- * - receives a number n
- * - returns its square if it's odd
- * - returns its double if it's even
- *
- * e.g.
- * squareOrDouble(16) -> 32
- * squareOrDouble(9) -> 81
- */
 function squareOrDouble(n) {
   if (isOdd(n)) return n ** 2;
   else return n * 2;
